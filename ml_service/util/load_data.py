0ace595850a6ab9f12abb1f3c8d6ef8ea33905cd
sumantha-NTS <sumanthneechadi@gmail.com>
2021-08-19 20:29:26 +0530

    dataset uploaded

--- ml_service/util/load_data.py
+++ ml_service/util/load_data.py
@@ -2,5 +2,5 @@ import pandas as pd
 
 
 def upload_from_local(file_name: str = 'heart_fail.csv'):
-    df = pd.read_csv('**heart_failure.csv')
+    df = pd.read_csv('../../classification/data/heart_failure.csv')
     df.to_csv(file_name)
