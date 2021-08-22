858614ac2caca01778d0e8851b674208c2ec347d
sumantha-NTS <sumanthneechadi@gmail.com>
2021-08-22 10:08:04 +0530

    changes in score.py file

--- classification/scoring/score.py
+++ classification/scoring/score.py
@@ -1,5 +1,5 @@
+import numpy
 import joblib
-import numpy as np
 import os
 from azureml.core.model import Model
 from inference_schema.schema_decorators \
@@ -7,19 +7,52 @@ from inference_schema.schema_decorators \
 from inference_schema.parameter_types.numpy_parameter_type \
     import NumpyParameterType
 
-input_sample = np.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
-    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
-output_sample = np.array([
-    5021.509689995557,
-    3693.645386402646])
 
-
-@input_schema('data', NumpyParameterType(input_sample))
-@output_schema(NumpyParameterType(output_sample))
 def init():
+    # load the model from file into a global object
     global model
+
+    # we assume that we have just one model
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder
+    # (./azureml-models/$MODEL_NAME/$VERSION)
     model_path = Model.get_model_path(
         os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
 
     model = joblib.load(model_path)
+
+
+input_sample = numpy.array([
+    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
+output_sample = numpy.array([
+    5021.509689995557,
+    3693.645386402646])
+
+
+# Inference_schema generates a schema for your web service
+# It then creates an OpenAPI (Swagger) specification for the web service
+# at http://<scoring_base_url>/swagger.json
+@input_schema('data', NumpyParameterType(input_sample))
+@output_schema(NumpyParameterType(output_sample))
+def run(data, request_headers):
+    result = model.predict(data)
+
+    print(('{{"RequestId":"{0}", '
+           '"TraceParent":"{1}", '
+           '"NumberOfPredictions":{2}}}'
+           ).format(
+               request_headers.get("X-Ms-Request-Id", ""),
+               request_headers.get("Traceparent", ""),
+               len(result)
+    ))
+
+    return {"result": result.tolist()}
+
+
+if __name__ == "__main__":
+    # Test scoring
+    init()
+    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10,11,12],[12,11,10,9,8,7,6,5,4,3,2,1]]}'
+    prediction = run(test_row, {})
+    print("Test result: ", prediction)
