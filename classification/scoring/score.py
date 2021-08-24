1f25ff5d5ee0ed63aeee7f10766581b4459fcf6a
sumantha-NTS <sumanthneechadi@gmail.com>
2021-08-24 17:35:20 +0530

    changes in score file

--- classification/scoring/score.py
+++ classification/scoring/score.py
@@ -1,11 +1,12 @@
+
 import numpy
 import joblib
 import os
 from azureml.core.model import Model
-# from inference_schema.schema_decorators \
-#     import input_schema, output_schema
-# from inference_schema.parameter_types.numpy_parameter_type \
-#     import NumpyParameterType
+from inference_schema.schema_decorators \
+    import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type \
+    import NumpyParameterType
 
 
 def init():
@@ -23,8 +24,8 @@ def init():
 
 
 input_sample = numpy.array([
-    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11, 12],
-    [12, 11, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
+    [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
+    [10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0, 2.0, 1.0]])
 output_sample = numpy.array([
     0,
     1])
@@ -33,18 +34,26 @@ output_sample = numpy.array([
 # Inference_schema generates a schema for your web service
 # It then creates an OpenAPI (Swagger) specification for the web service
 # at http://<scoring_base_url>/swagger.json
-# @input_schema('data', NumpyParameterType(input_sample))
-# @output_schema(NumpyParameterType(output_sample))
+@input_schema('data', NumpyParameterType(input_sample))
+@output_schema(NumpyParameterType(output_sample))
 def run(data, request_headers):
-    result = model.predict(data)
-    return {"result": result}
+    # result = model.predict(data)
+
+    print(('{{"RequestId":"{0}", '
+           '"TraceParent":"{1}", '
+           '"NumberOfPredictions":{2}}}'
+           ).format(
+               request_headers.get("X-Ms-Request-Id", ""),
+               request_headers.get("Traceparent", ""),
+               len(result)
+    ))
+
+    return {"result": 'success'}
 
 
 if __name__ == "__main__":
     # Test scoring
     init()
-    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10,11,12], '
-    '[12,11,10,9,8,7,6,5,4,3,2,1]]}'
+    test_row = '{"data":[[1,2,3,4,5,6,7,8,9,10],[10,9,8,7,6,5,4,3,2,1]]}'
     prediction = run(test_row, {})
-    # res = requests.get(url,data = prediction)
     print("Test result: ", prediction)
