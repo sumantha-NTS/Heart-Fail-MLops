c0713959941504b787e7e8fbb6e1c43d63a13086
sumantha-NTS <sumanthneechadi@gmail.com>
2021-08-24 16:42:30 +0530

    changes in score file

--- classification/scoring/app.py
+++ classification/scoring/app.py
@@ -1,7 +1,23 @@
 from flask import Flask
+import joblib
+from azureml.core.model import Model
+import os
 
 app = Flask(__name__)
 
+
+def init():
+    # load the model from file into a global object
+    global model
+
+    # we assume that we have just one model
+    # AZUREML_MODEL_DIR is an environment variable created during deployment.
+    # It is the path to the model folder
+    # (./azureml-models/$MODEL_NAME/$VERSION)
+    model_path = Model.get_model_path(
+        os.getenv("AZUREML_MODEL_DIR").split('/')[-2])
+
+    model = joblib.load(model_path)
 
 @app.route('/')
 def index():
@@ -9,4 +25,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run()
+    init()
